61ba8926308a1c5280c73888989575175e19e3bc
MikeDevUcamp <115725749+MikeDevUcamp@users.noreply.github.com>
2023-03-28 20:32:50 -0600

    user class

--- login412/User.swift
+++ login412/User.swift
@@ -1,1 +1,25 @@
+class User{
+    var username:String
+    var password:String
+    
+    init(_ username:String, _ password:String) {
+        self.username = username
+        self.password = password
+    }
+    
+}
 
+class LoginController{
+    var users = [User]()
+    
+    func addUser(_ user:User){
+        users.append(user)
+    }
+    
+    func login(_ username:String, _ password:String) ->  Bool {
+        for user in users {
+            return user.username == username && user.password == password
+        }
+        return false
+    }
+}
